427ca3fd4b768290172824a3e65fe30fcd152943
JESUS <sakrateesh007@gmail.com>
2022-06-11 10:19:21 +0530

    Admin functionality 2

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 
 const router = jsonServer.router("./db.json");
-
+const cors = require("cors");
 const middleware = jsonServer.defaults({
     static:"./build"
 });
@@ -14,6 +14,7 @@ server.use(
     })
 );
 server.use(router);
+server.use(cors());
 server.listen(PORT,() =>{
     console.log("server is live at PORT", PORT);
 })
